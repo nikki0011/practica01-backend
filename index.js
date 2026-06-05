58e333d8395b505a180b180bddd0dc30776f60b9
Nair Paez <nairpaez@outlook.com>
2026-06-05 19:58:14 -0300

    agregamos los endpoints y probamos en postman

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 // const express = require('express')
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 //agregar los middlewares
-app.use(cors())// permite conexiones remotas
-app.use(express.json())//permite interpretar los datos que llegan en la solicitud en formato json
-// agregar el otro middleware morgan
+app.use(cors()); // permite conexiones remotas
+app.use(express.json()); //permite interpretar los datos que llegan en la solicitud en formato json
+app.use(morgan("dev"));
 
-console.log('Este backend ya escucha')
+console.log("Este backend ya escucha");
 
-app.listen(PORT,()=>{console.info(`Servidor activo en el puerto ${PORT}`)})
+// endpoints
+//http://localhost:3000/api/saludo
+app.get("/api/saludo", (req, res) => {
+  console.log("me llego una solicitud nueva");
+  const comida = ["manzana", "pera", "otra manzana"];
+  res.json({
+    mensaje: "Conexion creada",
+    saludo: "Bienvenido a nuestro backend",
+    comida,
+  });
+});
+//http://localhost:3000/api/productos
+app.get("/api/productos", (req, res) => {
+  console.log("me llego una solicitud nueva");
+  const productos = ["Cappuccino", "Medialuna", "Tostado de jamón y queso"];
+  res.json({
+    productos,
+  });
+});
+//http://localhost:3000/api/promo
+app.get("/api/promo", (req, res) => {
+  console.log("me llego una solicitud nueva");
+  res.json("¡Hoy 2x1 en licuados de frutilla!");
+});
+//http://localhost:3000/api/horarios
+app.get("/api/horarios", (req, res) => {
+  console.log("me llego una solicitud nueva");
+  const horarios =`
+  De lunes a sabados de 08.00hs a 02.00hs- Domingos de 08.00hs a 00.00hs` 
+  res.json({
+    horarios},
+  );
+});
+
+app.listen(PORT, () => {
+  console.info(`Servidor activo en el puerto ${PORT}`);
+});
