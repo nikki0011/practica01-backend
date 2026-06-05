d052a76a4a0e3b933010793ef88be14848789572
Nair Paez <nairpaez@outlook.com>
2026-06-05 18:11:08 -0300

    agregamos los middlewares del proyecto

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // const express = require('express')
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
+
+//agregar los middlewares
+app.use(cors())// permite conexiones remotas
+app.use(express.json())//permite interpretar los datos que llegan en la solicitud en formato json
+// agregar el otro middleware morgan
 
 console.log('Este backend ya escucha')
 
